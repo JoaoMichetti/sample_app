e4d1c72c1d933b7636bdf50ed4782c0dad033a2c
Joao Michetti <michettimatos@gmail.com>
2020-03-27 15:19:22 -0300

    Fazendo um modelo de user Básico com senhas seguras

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "example user", email:"user@example.com")
+    @user = User.new(name: "example user", email:"user@example.com",
+                      password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -55,5 +56,21 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user = @user.dup
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExaMple.Com"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimun length" do
+    @user.password = @user.password_confirmation = "a" * 5
   end
 end
